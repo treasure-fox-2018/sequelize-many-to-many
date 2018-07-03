733f5247f9e5072ccbb5d06e87162c52b207deac
dawimuhammad <muhamadhaddawi@gmail.com>
2018-07-03 18:31:27 +0700

    Release 2 - Enrolled Student Feature, Add score student still need to be fixed

--- models/subjectstudent.js
+++ models/subjectstudent.js
@@ -1,48 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var SubjectStudent = sequelize.define('SubjectStudent', {
-    StudentId: DataTypes.INTEGER,
-    SubjectId: DataTypes.INTEGER
-  }, {});
+      StudentId: DataTypes.INTEGER,
+      SubjectId: DataTypes.INTEGER,
+      score: DataTypes.INTEGER
+    }, {}
+  )
+
   SubjectStudent.associate = function(models) {
       SubjectStudent.belongsTo(models.Student)
       SubjectStudent.belongsTo(models.Subject)
-  };
+  }
+
   return SubjectStudent;
-};
-
-
-//
-// routes.post('/teacher/add', (req, res) => {
-//     let teacherFirstname = req.body.firstName
-//     let teacherLastname = req.body.lastName
-//     let teacherEmailArgs = req.body.email
-//
-//     Subject.findOne({where: {subjectName: req.body.subjectChoose}})
-//     .then(subject => {
-//         Teacher.findAll({
-//                 where: {
-//                   email: teacherEmailArgs}
-//             })
-//             .then(emailExists => {
-//                 if (emailExists.length >= 1) {
-//                     console.log('Email sudah ada!')
-//                     res.redirect('/teacher')
-//                 } else {
-//                   Teacher.create({
-//                       firstName: teacherFirstname,
-//                       lastName: teacherLastname,
-//                       email: teacherEmailArgs,
-//                       SubjectId: subject.id,
-//                   })
-//                   .then(() => {
-//                       res.redirect('/teacher')
-//                   })
-//                   .catch(err => {
-//                       console.log(err)
-//                   })
-//                 }
-//             })
-//             .catch(err => console.log(err))
-//     })
-// })
+}
--- routes/index.js
+++ routes/index.js
@@ -106,142 +106,86 @@ routes.post('/student/:id/add-subject', (req, res) => {
             console.log(err)}
         )
     })
-
 })
 
+routes.get('/subject', (req, res) => {
+    Subject.findAll()
+    .then(subjects => {
+        res.render('subject', {subjects: subjects})
+    })
+})
 
+routes.get('/subject/:subjectId/enrolled-students', (req, res) => {
+    SubjectStudent.findAll(
+        {include: [Student], where: {SubjectId: req.params.subjectId}})
+    .then(StudentsBySubject => {
+        // res.send(StudentsBySubject)
+        console.log(JSON.stringify(StudentsBySubject))
+        res.render('enrolled-student', {students: StudentsBySubject})
+    })
+    .catch(err => console.log(err))
+})
 
-// routes.get('/subject/add', (req, res) => {
-//     res.render('subject-add')
-// })
-//
-// routes.post('/subject/add', (req, res) => {
-//     Subject.findAndCountAll({where: {subjectName: req.body.subjectName}})
-//     .then(findSubjectResult => {
-//         if (findSubjectResult >= 1) {
-//             console.log('Subject sudah ada')
-//             res.redirect('/subject/add')
-//         } else {
-//             Subject.create(
-//             {
-//                   subjectName: req.body.subjectName
-//             })
-//             .then( () => {
-//                 res.redirect('/subject')
-//             })
-//             .catch(err => console.log(err))
-//         }
-//     })
-//     .catch(err => console.log(err))
-// })
-//
-// routes.get('/teacher', (req, res) => {
-//     Teacher.findAll({include: [Subject]})
-//     .then(teachers => {
-//         res.render('teacher', {teachers: teachers})
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-// })
-//
-// routes.get('/teacher/add', (req, res) => {
-//     Subject.findAll()
-//     .then(subjects => {
-//         res.render('teacher-add', {subjects: subjects})
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-// })
-//
-// routes.post('/teacher/add', (req, res) => {
-//     let teacherFirstname = req.body.firstName
-//     let teacherLastname = req.body.lastName
-//     let teacherEmailArgs = req.body.email
-//
-//     Subject.findOne({where: {subjectName: req.body.subjectChoose}})
-//     .then(subject => {
-//         Teacher.findAll({
-//                 where: {
-//                   email: teacherEmailArgs}
-//             })
-//             .then(emailExists => {
-//                 console.log(emailExists)
-//
-//                 if (emailExists.length >= 1) {
-//                     console.log('Email sudah ada!')
-//                     res.redirect('/teacher')
-//                 } else {
-//                   Teacher.create({
-//                       firstName: teacherFirstname,
-//                       lastName: teacherLastname,
-//                       email: teacherEmailArgs,
-//                       SubjectId: subject.id,
-//                   })
-//                   .then(() => {
-//                       res.redirect('/teacher')
-//                   })
-//                   .catch(err => {
-//                       console.log(err)
-//                   })
-//                 }
-//             })
-//             .catch(err => console.log(err))
-//     })
-// })
-//
-// routes.get('/teacher/delete/:id', (req, res) => {
-//     Teacher.destroy({
-//         where: {id: req.params.id}
-//     })
-//     .then(() =>
-//         res.redirect('/teacher')
-//     )
-//     .catch(err => console.log(err))
-// })
-//
-// routes.get('/teacher/edit/:teacherId', (req, res) => {
-//     let teacherId = req.params.teacherId
-//
-//     Subject.findAll()
-//     .then(subjects => {
-//         Teacher.findById(teacherId)
-//         .then(teacher => {
-//           res.render('edit-teacher', {teacher:teacher, subjects:subjects})
-//         })
-//         .catch(err => console.log(err))
-//     })
-// })
-//
-// routes.post('/teacher/edit/:teacherId', (req, res) => {
-//     let teacherId = req.params.teacherId
-//
-//     let newTeacherFirstname = req.body.firstName
-//     let newTeacherLastname = req.body.lastName
-//     let newTeacherEmail = req.body.email
-//
-//     Teacher.update(
-//       {
-//         firstName: newTeacherFirstname,
-//         lastName: newTeacherLastname,
-//         email: newTeacherEmail,
-//       }, {where: {id : teacherId}}
-//     )
-//     .then(() =>
-//         res.redirect('/teacher')
-//     )
-//     .catch(err => console.log(err))
-// })
-//
-// routes.get('/subject', (req, res) => {
-//     Subject.findAll({include: [Teacher]})
-//     .then(subjects => {
-//         res.render('subject', {subjects: subjects})
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-// })
+routes.get('/subject/:StudentId/give-score', (req, res) => {
+  let msgerror = ''
+  Student.findById(req.params.StudentId)
+  .then(student => {
+      res.render('student-give-score', {student:student, msgerror:msgerror})
+  })
+})
+
+routes.post('/subject/:StudentId/give-score', (req, res) => {
+    let studentScore = req.body.score
+
+    if (studentScore > 100 || studentScore < 0) {
+        let msgerror = 'Score not valid!'
+        Student.findById(req.params.StudentId)
+        .then(student => {
+            res.render('student-give-score', {student:student, msgerror:msgerror})
+        })
+    } else {
+        SubjectStudent.update(
+          {
+            score: studentScore
+          },
+          {
+            where: {
+              id: req.params.StudentId
+            }
+          })
+        .then( () => {
+            res.redirect('/subject')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+})
 
 module.exports = routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
